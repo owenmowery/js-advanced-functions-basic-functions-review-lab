b5aa9035b767f51aa494929dc9bf56a50aa7ceec
Owen Mowery <owenmowery99@gmail.com>
2021-06-07 14:19:04 -0400

    commit tree a6a8ad5b3e714a4c6f7c03c21df085c83d2eb625

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+function saturdayFun(activity) {
+    if (activity == undefined) {
+        return "This Saturday, I want to roller-skate!"
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }
+};
+
+function mondayWork(activity) {
+    if (activity == undefined) {
+        return "This Monday, I will go to the office.";
+    } else {
+        return `This Monday, I will ${activity}.`;
+    }
+};
+
+function wrapAdjective(flair="*") {
+    return function(adj="special", ) {
+        return `You are ${flair}${adj}${flair}!`;
+    }
+};
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2;
+    },
+    divide: function(num1, num2) {
+        return num1 / num2;
+    }
+};
+
+function actionApplyer(integer, arrOfFunc) {
+    if (arrOfFunc.length === 0) {
+        return integer;
+    } else {
+        for (const func of arrOfFunc) {
+            integer = func(integer);
+        }
+        return integer;
+    }
+};
